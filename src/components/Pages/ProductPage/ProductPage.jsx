a5f9d3849bff7b88e08022bf18baf0684de8bc22
prdg <prdg990515@gmail.com>
2022-12-22 15:18:23 +0900

    크기큰 화면에서 중앙정렬

--- src/components/Pages/ProductPage/ProductPage.jsx
+++ src/components/Pages/ProductPage/ProductPage.jsx
@@ -17,58 +17,60 @@ const Postpage = () => {
     dispatch(__getProductpost());
   }, [dispatch]);
   return (
-    <div className="product">
-      <div className="product_visual">
-        {/* 메인 이미지 */}
-        <div className="product_visual_img">
-          <img src={product_post?.img} />
-        </div>
-        <div className="product_visual_text">
-          {/* 메인 비주얼 왼쪽 텍스트 */}
-          <div>
-            <h2>
-              {product_post?.title}
-              <br />
-              <span>{product_post?.engtitle}</span>
-            </h2>
+    <div className="product_middle">
+      <div className="product">
+        <div className="product_visual">
+          {/* 메인 이미지 */}
+          <div className="product_visual_img">
+            <img src={product_post?.img} />
+          </div>
+          <div className="product_visual_text">
+            {/* 메인 비주얼 왼쪽 텍스트 */}
             <div>
-              <h3>Price</h3>
-              <p>{product_post?.price}</p>
-              <button>Reservation</button>
+              <h2>
+                {product_post?.title}
+                <br />
+                <span>{product_post?.engtitle}</span>
+              </h2>
+              <div>
+                <h3>Price</h3>
+                <p>{product_post?.price}</p>
+                <button>Reservation</button>
+              </div>
+            </div>
+            {/* 메인 비주얼 오른쪽 텍스트 */}
+            <div>
+              <p>{product_post?.titleKrText}</p>
+              <p>{product_post?.titleEnText}</p>
             </div>
           </div>
-          {/* 메인 비주얼 오른쪽 텍스트 */}
+        </div>
+        {/* 상세페이지*/}
+        <div className="product_contents">
           <div>
-            <p>{product_post?.titleKrText}</p>
-            <p>{product_post?.titleEnText}</p>
+            <div className="product_box">
+              <span>
+                <img src={product_post?.detail1} />
+              </span>
+              <h2>{product_post?.detailTitle1}</h2>
+              <p>{product_post?.detailText1}</p>
+            </div>
+            <div className="product_box">
+              <span>
+                <img src={product_post?.detail3} />
+              </span>
+              <h2>{product_post?.detailTitle3}</h2>
+              <p>{product_post?.detailText3}</p>
+            </div>
           </div>
-        </div>
-      </div>
-      {/* 상세페이지*/}
-      <div className="product_contents">
-        <div>
-          <div className="product_box">
-            <span>
-              <img src={product_post?.detail1} />
-            </span>
-            <h2>{product_post?.detailTitle1}</h2>
-            <p>{product_post?.detailText1}</p>
-          </div>
-          <div className="product_box">
-            <span>
-              <img src={product_post?.detail3} />
-            </span>
-            <h2>{product_post?.detailTitle3}</h2>
-            <p>{product_post?.detailText3}</p>
-          </div>
-        </div>
-        <div>
-          <div className="product_box product_right_box">
-            <span>
-              <img src={product_post?.detail2} />
-            </span>
-            <h2>{product_post?.detailTitle2}</h2>
-            <p>{product_post?.detailText2}</p>
+          <div>
+            <div className="product_box product_right_box">
+              <span>
+                <img src={product_post?.detail2} />
+              </span>
+              <h2>{product_post?.detailTitle2}</h2>
+              <p>{product_post?.detailText2}</p>
+            </div>
           </div>
         </div>
       </div>
