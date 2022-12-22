1ea6358b550ac551592c0a1ccfae6dd733289848
go <sukpo6010@gmail.com>
2022-12-22 13:22:22 +0900

    topimage

--- src/components/Pages/PostListpage/PostsContainer.jsx
+++ src/components/Pages/PostListpage/PostsContainer.jsx
@@ -16,25 +16,63 @@ const PostsContainer = () => {
   const { posts } = useSelector((state) => state.posts);
 
   return (
-    <CommentsWrap>
-      <PostButtonWrap>
-        <CusttomButton
-          onClick={() => {
-            navigate("/addform");
-          }}
-        >
-          추가
-        </CusttomButton>
-      </PostButtonWrap>
+    <PostsWrap>
+      <TopimgWrap>
+        <Topimg></Topimg>
+        <Title>Reservation</Title>
+      </TopimgWrap>
+      <CommentsWrap>
+        <PostButtonWrap>
+          <CusttomButton
+            onClick={() => {
+              navigate("/addform");
+            }}
+          >
+            추가
+          </CusttomButton>
+        </PostButtonWrap>
 
-      {posts.map((post) => {
-        return <PostContainer key={post.id} post={post}></PostContainer>;
-      })}
-    </CommentsWrap>
+        {posts.map((post) => {
+          return <PostContainer key={post.id} post={post}></PostContainer>;
+        })}
+      </CommentsWrap>
+    </PostsWrap>
   );
 };
 
 export default PostsContainer;
+
+const Title = styled.div`
+  position: relative;
+  margin: 0 auto;
+  top: -30%;
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 100;
+  font-size: 40px;
+  line-height: 54px;
+`;
+
+const PostsWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const TopimgWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Topimg = styled.img.attrs({
+  src: "images/CummunityPageimg/top_img.png",
+})`
+  width: 100%;
+  min-height: 500px;
+  object-fit: cover;
+`;
 
 const CommentsWrap = styled.div`
   width: 1000px;
