2a994b519553567e407a56e57a887541c6422da5
sungmin <95615086+Clariatinet@users.noreply.github.com>
2022-12-22 13:27:34 +0900

    viewMore 이미지 추가

--- src/components/Pages/MainPage/Mainpage.jsx
+++ src/components/Pages/MainPage/Mainpage.jsx
@@ -8,6 +8,12 @@ export default function Mainpage() {
         N<br />U<br />L<br />L<br />B<br />A<br />K<br />E<br />R<br />Y
       </p>
       <img className='mainBread' src='images/HomePageimg/MainImg.jpg' alt='' />
+      <div className='viewMoreBox'>
+        <img src='images/HomePageimg/bread1.png' alt='' />
+        <img src='images/HomePageimg/bread2.png' alt='' />
+        <img src='images/HomePageimg/bread3.png' alt='' />
+        <img src='images/HomePageimg/bread4.png' alt='' />
+      </div>
     </body>
   );
 }
