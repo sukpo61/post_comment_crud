ee9d7ba9ac474c50650ec04bd42f7f5d6301768b
YoungJae0910 <dbdudwo0910@gmail.com>
2022-12-22 11:59:51 +0900

    sungmin

--- src/shared/router.js
+++ src/shared/router.js
@@ -1,28 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styled from "styled-components";
-import PostsContainer from "../components/Pages/PostListpage/PostsContainer";
 import Header from "../components/Layout/Header";
-import PostAddForm from "../components/Pages/PostAddpage/PostAddForm";
-import Postpage from "../components/Pages/Postpage/Postpage";
-import PostEditForm from "../components/Pages/PostEditpage/PostEditForm";
-
-const Wrap = styled.div`
-  width: 70%;
-  min-width: 500px;
-  margin: 100px auto;
-  display: flex;
-`;
+import Mainpage from "../components/Pages/MainPage/Mainpage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<PostsContainer />} />
-        <Route path="/addform" element={<PostAddForm />} />
-        <Route path="/editform" element={<PostEditForm />} />
-        <Route path="/:id" element={<Postpage />} />
+        <Route path="/" element={<Mainpage />} />
       </Routes>
     </BrowserRouter>
   );
