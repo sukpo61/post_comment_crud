fdda0773bfa64b70df9da90493ccab2f8156394c
sungmin <95615086+Clariatinet@users.noreply.github.com>
2022-12-22 17:01:30 +0900

    header z-index 수정

--- src/shared/router.js
+++ src/shared/router.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "../components/Pages/Footer/Footer";
-import Header from "../components/Layout/Header";
-import Mainpage from "../components/Pages/MainPage/Mainpage";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
+import Mainpage from '../components/Pages/MainPage/Mainpage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<Mainpage />} />
+        <Route path='/' element={<Mainpage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
