aa43d55396d6ee9e4f0b45e36e4334503423fadf
YoungJae0910 <dbdudwo0910@gmail.com>
2022-12-22 13:45:01 +0900

    footer finished

--- src/components/Pages/MainPage/Mainpage.jsx
+++ src/components/Pages/MainPage/Mainpage.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export default function Mainpage() {
   return (
     <body>
-      <p className='MainText'>
+      <p className="MainText">
         N<br />U<br />L<br />L<br />B<br />A<br />K<br />E<br />R<br />Y
       </p>
-      <img className='mainBread' src='images/HomePageimg/MainImg.jpg' alt='' />
-      <div className='viewMoreBox'>
-        <img src='images/HomePageimg/bread1.png' alt='' />
-        <img src='images/HomePageimg/bread2.png' alt='' />
-        <img src='images/HomePageimg/bread3.png' alt='' />
-        <img src='images/HomePageimg/bread4.png' alt='' />
-      </div>
+      <img className="mainBread" src="images/HomePageimg/MainImg.jpg" alt="" />
+      {/* <div className='viewMoreBox'> */}
+      <img src="images/HomePageimg/bread1.png" alt="" />
+      <img src="images/HomePageimg/bread2.png" alt="" />
+      <img src="images/HomePageimg/bread3.png" alt="" />
+      <img src="images/HomePageimg/bread4.png" alt="" />
+      {/* </div> */}
     </body>
   );
 }
