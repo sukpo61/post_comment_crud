d594e8c2f35d7e44021f4509e6086c3b9b949cfb
YoungJae0910 <dbdudwo0910@gmail.com>
2022-12-22 16:00:43 +0900

    modal

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import './style.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./style.css";
+import Modal from "../Pages/Modal/Modal";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   const navigate = useNavigate();
   return (
-    <header className='Headerbox'>
+    <header className="Headerbox">
       <div
         onClick={() => {
-          navigate('/');
+          navigate("/");
         }}
       >
-        <img className='HeaderLogo' src='images/HomePageimg/Logo.png' alt='' />
+        <a href="">
+          <img
+            className="HeaderLogo"
+            src="images/HomePageimg/Logo.png"
+            alt=""
+          />
+        </a>
       </div>
-      <div className='Hambuger'>
-        <i class='fa-solid fa-bars'></i>
+      <div className="Hambuger">
+        <i
+          class="fa-solid fa-bars"
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        ></i>
+        <Modal open={openModal} onClose={() => setOpenModal(false)} />
       </div>
     </header>
   );
