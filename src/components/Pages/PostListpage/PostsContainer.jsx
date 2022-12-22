a33c64b07362964d9c81ed907bbc01d083a547e4
go <sukpo6010@gmail.com>
2022-12-22 11:21:26 +0900

    headerbring

--- src/components/Pages/PostListpage/PostsContainer.jsx
+++ src/components/Pages/PostListpage/PostsContainer.jsx
@@ -3,9 +3,12 @@ import styled from "styled-components";
 import PostContainer from "./PostContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { __getPost } from "../../../redux/modules/posts";
+import { useLocation, useNavigate } from "react-router-dom";
+
 
 const PostsContainer = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(__getPost());
@@ -15,6 +18,14 @@ const PostsContainer = () => {
 
   return (
     <CommentsWrap>
+      <PostButtonWrap>
+
+ <CusttomButton onClick={()=>{
+navigate("/addform")
+ }}>추가</CusttomButton>
+
+ </PostButtonWrap>
+
       {posts.map((post) => {
         return <PostContainer key={post.id} post={post}></PostContainer>;
       })}
@@ -27,8 +38,23 @@ export default PostsContainer;
 const CommentsWrap = styled.div`
   width: 1000px;
   min-width: 500px;
-  margin: 100px auto;
-    display: flex;
+  margin: 0 auto;
+  display: flex;
   flex-direction: column;
 `;
 
+const PostButtonWrap = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+`;
+
+const CusttomButton = styled.button`
+  width: 50px;
+  height: 30px;
+  cursor: pointer;
+  border: 1px solid #eee;
+`;
