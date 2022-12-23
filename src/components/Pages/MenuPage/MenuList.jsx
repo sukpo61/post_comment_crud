b6cb7d9a8cc901569fe29e8f474a0046a33b0d3d
go <sukpo6010@gmail.com>
2022-12-23 09:38:10 +0900

    toolkit적용할것

--- src/components/Pages/MenuPage/MenuList.jsx
+++ src/components/Pages/MenuPage/MenuList.jsx
@@ -1,41 +1,50 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
 
 function MenuList() {
   // store에 있는 products들을 가져와야 함
-  const products = useSelector((state) => state.products);
-  const [currentCategory, setCurrentCategory] = useState('Bread');
-
-  const changeCategory = (c) => {
-    setCurrentCategory(c);
-    console.log(currentCategory);
-  };
+  const { products } = useSelector((state) => state.products);
+  const [currentCategory, setCurrentCategory] = useState("Bread");
+  console.log(products);
+  // const changeCategory = (c) => {
+  //   // setCurrentCategory(c);
+  //   // console.log(currentCategory);
+  // };
 
   return (
-    <>
+    <div>
       <StyledCategroyBox>
-        <div onClick={changeCategory('Bread')}>Bread</div>
-        <div onClick={changeCategory('Cake')}>Cake</div>
-        <div onClick={changeCategory('Cookie')}>Cookie</div>
-        <div onClick={changeCategory('Coffee')}>Coffee</div>
+        <div onClick={setCurrentCategory("Bread")}>Bread</div>
+        <div onClick={setCurrentCategory("Cake")}>Cake</div>
+        <div onClick={setCurrentCategory("Cookie")}>Cookie</div>
+        <div onClick={setCurrentCategory("Coffee")}>Coffee</div>
       </StyledCategroyBox>
       <StyledListBox>
         {/* products중에 카테고리가 bread인 아이템들만 가져와서 */}
-        {products
-          .filter((item) => item.category === 'Bread')
-          .map((item) => {
-            return (
-              <StyledPoductsBox>
+        {products.map(
+          (item) =>
+            item.category === currentCategory && (
+              <StyledPoductsBox key={item.id}>
                 <img src={item.img} />
                 <p>{item.name}</p>
               </StyledPoductsBox>
-            );
-          })}
+            )
+        )}
       </StyledListBox>
-    </>
+    </div>
   );
 }
+// {products
+//   .filter((item) => item.category === "Bread")
+//   .map((item) => {
+//     return (
+//       <StyledPoductsBox>
+//         <img src={item.img} />
+//         <p>{item.name}</p>
+//       </StyledPoductsBox>
+//     );
+//   })}
 
 export default MenuList;
 
