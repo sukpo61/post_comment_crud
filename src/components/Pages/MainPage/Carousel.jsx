aea8b33b58181d2e4b79b24da6e69084235e899c
sungmin <95615086+Clariatinet@users.noreply.github.com>
2022-12-23 11:41:18 +0900

    이미지 슬라이드 추가

---
+++ src/components/Pages/MainPage/Carousel.jsx
@@ -0,0 +1,26 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+
+const Carousel = ({ img1, img2, img3, img4, img5 }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const arr = [img1, img2, img3, img4, img5];
+  return (
+    <div className='carousel'>
+      <Slider {...settings}>
+        {arr.map((item) => {
+          return <img src={item} alt='반죽' />;
+        })}
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/Pages/MainPage/Mainpage.jsx
+++ src/components/Pages/MainPage/Mainpage.jsx
@@ -1,65 +1,90 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Carousel from './Carousel';
 
 export default function Mainpage() {
   return (
     <body>
-      <p className="MainText">
+      <p className='MainText'>
         N<br />U<br />L<br />L<br />B<br />A<br />K<br />E<br />R<br />Y
       </p>
       <img
-        className="mainBread"
-        src="images/HomePageimg/changeMain.png"
-        alt=""
+        className='mainBread'
+        src='images/HomePageimg/changeMain.png'
+        alt=''
       />
-      <div className="background">
-        <img className="bakery" src="images/HomePageimg/toast.png" alt="빵집" />
+      <div className='carousels'>
+        <Carousel
+          img1={'images/HomePageimg/img1.png'}
+          img2={'images/HomePageimg/img2.png'}
+          img3={'images/HomePageimg/img3.png'}
+          img4={'images/HomePageimg/img4.png'}
+          img5={'images/HomePageimg/img5.jpeg'}
+        />
+        <Carousel
+          img1={'images/HomePageimg/img1.png'}
+          img2={'images/HomePageimg/img2.png'}
+          img3={'images/HomePageimg/img3.png'}
+          img4={'images/HomePageimg/img4.png'}
+          img5={'images/HomePageimg/img5.jpeg'}
+        />
       </div>
-      <img className="wheat" src="images/HomePageimg/wheat.png" alt="밀밭" />
+      <div className='background'>
+        <img className='bakery' src='images/HomePageimg/man.png' alt='빵집' />
+      </div>
+      <div className='back'></div>
+      <img className='wheat' src='images/HomePageimg/wheat.png' alt='밀밭' />
+      <div className='wheat__'>
+        <p className='wheat__p1'>우리의 밀로</p>
+        <p className='wheat__p2'>우리의 것으로</p>
+        <p className='wheat__p3'>우리의 미래를</p>
+        <p className='wheat__p4'>만듭니다</p>
+        <p className='wheat__p5'>후나</p>
+      </div>
       {/* <div style={{ position: "relative", height: "50px" }}></div> */}
-      <div class="work__projects">
-        <a href="#" class="project" target="blank">
+      <div class='work__projects'>
+        <a href='#' className='project' target='blank'>
           <img
-            src="images/HomePageimg/bread1.png"
-            alt="빵"
-            class="project__img"
+            src='images/HomePageimg/bread1.png'
+            alt='빵'
+            className='project__img'
           />
-          <div class="project__description">
+          <div className='project__description'>
             <h3>View more</h3>
             <span>Bread</span>
           </div>
         </a>
-        <div class="project" target="blank"></div>
-        <a href="#" class="project" target="blank">
+        <div className='project' target='blank'></div>
+        <a href='#' className='project' target='blank'>
           <img
-            src="images/HomePageimg/bread2.png"
-            alt="빵"
-            class="project__img"
+            src='images/HomePageimg/bread2.png'
+            alt='빵'
+            className='project__img'
           />
-          <div class="project__description">
+          <div className='project__description'>
             <h3>View more</h3>
             <span>Bread</span>
           </div>
         </a>
-        <a href="#" class="project" target="blank">
+        <a href='#' className='project' target='blank'>
           <img
-            src="images/HomePageimg/bread3.png"
-            alt="빵"
-            class="project__img"
+            src='images/HomePageimg/bread3.png'
+            alt='빵'
+            className='project__img'
           />
-          <div class="project__description">
+          <div className='project__description'>
             <h3>View More</h3>
             <span>Bread</span>
           </div>
         </a>
-        <div class="project" target="blank"></div>
-        <a href="#" class="project" target="blank">
+        <div className='project' target='blank'></div>
+        <a href='#' className='project' target='blank'>
           <img
-            src="images/HomePageimg/bread4.png"
-            alt="빵"
-            class="project__img"
+            src='images/HomePageimg/bread4.png'
+            alt='빵'
+            className='project__img'
           />
-          <div class="project__description">
+          <div className='project__description'>
             <h3>View More</h3>
             <span>Bread</span>
           </div>
