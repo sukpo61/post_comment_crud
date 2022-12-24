b6aaf382b380afb6bef403e1b93653aab65ec903
MarinaNam <namma123@naver.com>
2022-12-25 03:02:05 +0900

    메뉴페이지 2차 수정

--- src/components/Pages/MenuPage/MenuList.jsx
+++ src/components/Pages/MenuPage/MenuList.jsx
@@ -26,11 +26,22 @@ function MenuList() {
 
   return (
     <div>
+      <StyledHeader>
+        <div>MENU</div>
+      </StyledHeader>
       <StyledCategroyBox>
-        <div onClick={handleClick('bread')}>Bread</div>
-        <div onClick={handleClick('cake')}>Cake</div>
-        <div onClick={handleClick('cookie')}>Cookie</div>
-        <div onClick={handleClick('coffee')}>Coffee</div>
+        <div onClick={handleClick('bread')} style={{ cursor: 'pointer' }}>
+          Bread
+        </div>
+        <div onClick={handleClick('cake')} style={{ cursor: 'pointer' }}>
+          Cake
+        </div>
+        <div onClick={handleClick('cookie')} style={{ cursor: 'pointer' }}>
+          Cookie
+        </div>
+        <div onClick={handleClick('coffee')} style={{ cursor: 'pointer' }}>
+          Coffee
+        </div>
       </StyledCategroyBox>
       <StyledListBox>
         {product_posts.map(
@@ -49,11 +60,19 @@ function MenuList() {
 
 export default MenuList;
 
+// 헤더랑 겹쳐서 분리하는 작업을 해줘야함!!
+const StyledHeader = styled.div`
+  background-color: white;
+  padding-top: 100px;
+  margin: 20px;
+  color: black;
+  font-size: 50px;
+`;
+
 // 카테고리 바
 const StyledCategroyBox = styled.div`
-  background-color: aqua;
-  padding: 100px;
-
+  background-color: white;
+  padding-top: 20px;
   display: flex;
   justify-content: space-around;
 
@@ -63,21 +82,26 @@ const StyledCategroyBox = styled.div`
 // 제품들 보여지는
 const StyledListBox = styled.div`
   background-color: beige;
-  padding: 200px;
+  padding: 20px;
   display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  /* grid-template-columns: 200px minmax(420px, 1fr);
+  grid-template-rows: 56px 1fr;
+} */
+  gap: 20px 10px;
 `;
 
 // 제품 하나 하나  - 이미지, 이름 세트 - 마우스 호버
 const StyledPoductsBox = styled.div`
-  background-color: beige;
-  /* padding: 20px;
-  margin: 5px; */
+  /* background-color: beige; */
+  padding: 20px;
+  margin: 5px;
   width: 100%;
-
   align-content: center;
-  grid-template-columns: 200px 200px 200px;
-  grid-auto-rows: minmax(100px, auto);
+  /* grid-template-columns: 200px 200px 200px; */
+  /* grid-auto-rows: minmax(100px, auto); */
   /* width: 224px;
   height: 224px; */
-  resize: both;
+  /* resize: both; */
 `;
