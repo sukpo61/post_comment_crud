0cc88ae3957f4dead2bb52e4d7cf7501a22a9155
MarinaNam <namma123@naver.com>
2022-12-25 14:17:15 +0900

    MenuPage.css 파일 생성. 미완성

--- src/components/Pages/MenuPage/MenuList.jsx
+++ src/components/Pages/MenuPage/MenuList.jsx
@@ -25,84 +25,66 @@ function MenuList() {
   };
 
   return (
-    <div>
-      <StyledHeader>
-        <div>MENU</div>
-      </StyledHeader>
-      <StyledCategroyBox>
-        <div onClick={handleClick('bread')} style={{ cursor: 'pointer' }}>
-          Bread
-        </div>
-        <div onClick={handleClick('cake')} style={{ cursor: 'pointer' }}>
-          Cake
-        </div>
-        <div onClick={handleClick('cookie')} style={{ cursor: 'pointer' }}>
-          Cookie
-        </div>
-        <div onClick={handleClick('coffee')} style={{ cursor: 'pointer' }}>
-          Coffee
-        </div>
-      </StyledCategroyBox>
-      <StyledListBox>
+    <div className='menu_page'>
+      <h1 className='menu_header'>MENU</h1>
+      <div className='menu_bar' onClick={handleClick('bread')}>
+        Bread
+      </div>
+      <div className='menu_bar' onClick={handleClick('cake')}>
+        Cake
+      </div>
+      <div className='menu_bar' onClick={handleClick('cookie')}>
+        Cookie
+      </div>
+      <div className='menu_bar' onClick={handleClick('coffee')}>
+        Coffee
+      </div>
+      <div className='menu_list'>
         {product_posts.map(
           (item) =>
             item.productMenu === currProductMenu && (
-              <StyledPoductsBox key={item.id}>
+              <div className='product' key={item.id}>
                 <img src={item.detail2} />
                 <p>{item.title}</p>
-              </StyledPoductsBox>
+              </div>
             )
         )}
-      </StyledListBox>
+      </div>
     </div>
+
+    // <div>
+    //   <StyledHeader>
+    //     <div>MENU</div>
+    //   </StyledHeader>
+    //   <StyledCategroyBox>
+    //     <div onClick={handleClick('bread')} style={{ cursor: 'pointer' }}>
+    //       Bread
+    //     </div>
+    //     <div onClick={handleClick('cake')} style={{ cursor: 'pointer' }}>
+    //       Cake
+    //     </div>
+    //     <div onClick={handleClick('cookie')} style={{ cursor: 'pointer' }}>
+    //       Cookie
+    //     </div>
+    //     <div onClick={handleClick('coffee')} style={{ cursor: 'pointer' }}>
+    //       Coffee
+    //     </div>
+    //   </StyledCategroyBox>
+    //   <StyledListBox>
+    //     {product_posts.map(
+    //       (item) =>
+    //         item.productMenu === currProductMenu && (
+    //           <StyledPoductsBox key={item.id}>
+    //             <StyledImg>
+    //               <img src={item.detail2} />
+    //             </StyledImg>
+    //             <p>{item.title}</p>
+    //           </StyledPoductsBox>
+    //         )
+    //     )}
+    //   </StyledListBox>
+    // </div>
   );
 }
 
 export default MenuList;
-
-// 헤더랑 겹쳐서 분리하는 작업을 해줘야함!!
-const StyledHeader = styled.div`
-  background-color: white;
-  padding-top: 100px;
-  padding-left: 75px;
-  margin: 20px;
-  color: black;
-  font-size: 50px;
-`;
-
-// 카테고리 바
-const StyledCategroyBox = styled.div`
-  background-color: white;
-  padding-top: 20px;
-  display: flex;
-  justify-content: space-around;
-
-  /* cursor: pointer; */
-`;
-
-// 제품들 보여지는
-const StyledListBox = styled.div`
-  background-color: beige;
-  padding: 20px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  /* grid-template-columns: 200px minmax(420px, 1fr);
-  grid-template-rows: 56px 1fr;
-} */
-  gap: 20px 10px;
-`;
-
-// 제품 하나 하나  - 이미지, 이름 세트 - 마우스 호버
-const StyledPoductsBox = styled.div`
-  /* background-color: beige; */
-  padding: 20px;
-  margin: 5px;
-  width: 100%;
-  align-content: center;
-  /* grid-template-columns: 200px 200px 200px; */
-  /* grid-auto-rows: minmax(100px, auto); */
-  /* width: 224px;
-  height: 224px; */
-  /* resize: both; */
-`;
