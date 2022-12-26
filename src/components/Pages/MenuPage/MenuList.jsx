b2d09639eec58f6534a5e090f49df42a4777ae0c
go <sukpo6010@gmail.com>
2022-12-26 12:38:45 +0900

    메뉴,제품페이지 병합완료

--- src/components/Pages/MenuPage/MenuList.jsx
+++ src/components/Pages/MenuPage/MenuList.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MenuPage.css';
-import { __getProductpost } from '../../../redux/modules/productposts';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./MenuPage.css";
+import { __getProductpost } from "../../../redux/modules/productposts";
+import { useNavigate } from "react-router";
 // useParams가져와서 props를 받아서 어쩌구 하면 헤더가 고정된다..............
 
 function MenuList() {
   // store에 있는 products들을 가져와야 함
   // 로컬에 저장된 state를 db.json으로 변경하는 함수 useEffect필요
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(__getProductpost());
@@ -16,8 +18,9 @@ function MenuList() {
   // 데이터 read해오는 함수
   const { product_posts } = useSelector((state) => state.product_posts);
 
-  const [currProductMenu, setCurrProductMenu] = useState('bread');
   console.log(product_posts);
+
+  const [currProductMenu, setCurrProductMenu] = useState("bread");
 
   // product_posts 저장값 중 하나인 productmenu값을 카테고리와 연결하여 state변경을 일으키는 함수
   const handleClick = (menu) => () => {
@@ -25,32 +28,40 @@ function MenuList() {
   };
 
   return (
-    <div className='menu_page'>
-      <div className='menu_bar'>
-        <div className='menu1' onClick={handleClick('bread')}>
+    <div className="menu_page">
+      <div className="menu_bar">
+        <div className="menu1" onClick={handleClick("bread")}>
           Bread
         </div>
-        <div className='menu2' onClick={handleClick('cake')}>
+        <div className="menu2" onClick={handleClick("cake")}>
           Cake
         </div>
-        <div className='menu3' onClick={handleClick('cookie')}>
+        <div className="menu3" onClick={handleClick("cookie")}>
           Cookie
         </div>
-        <div className='menu4' onClick={handleClick('coffee')}>
+        <div className="menu4" onClick={handleClick("coffee")}>
           Coffee
         </div>
       </div>
-      <div className='menu_list'>
-        {product_posts.map(
-          (item) =>
-            item.productMenu === currProductMenu && (
-              <div className='menu_product' key={item.id}>
-                <img className='image' src={item.detail3} />
-                <p className='title'>{item.title}</p>
-                <p className='engtitle'>{item.engtitle}</p>
+      <div className="menu_list">
+        {product_posts.map((item) => {
+          if (item.productmenu === currProductMenu) {
+            console.log(item);
+            return (
+              <div className="menu_product" key={item.id}>
+                <img
+                  className="image"
+                  src={item.detail2}
+                  onClick={() => {
+                    navigate(`/productpage/${item.id}`);
+                  }}
+                />
+                <p className="title">{item.title}</p>
+                <p className="engtitle">{item.engtitle}</p>
               </div>
-            )
-        )}
+            );
+          }
+        })}
       </div>
     </div>
   );
---
+++ src/redux/modules/productposts.js
@@ -0,0 +1,175 @@
+//제품불러오기
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const __getProductpost = createAsyncThunk(
+  "productposts/getproductpost",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.get("http://localhost:3003/product_posts");
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __addproduct_posts = createAsyncThunk(
+  "Productposts/addProductpost",
+  async (payload, thunkAPI) => {
+    try {
+      console.log(payload);
+      await axios.post("http://localhost:3003/product_posts", payload);
+      const data = await axios.get("http://localhost:3003/product_posts");
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __deleteproduct_posts = createAsyncThunk(
+  "Productposts/deleteProductpost",
+  async (payload, thunkAPI) => {
+    try {
+      console.log(payload);
+      await axios.delete(`http://localhost:3003/product_posts/${payload}`);
+      const data = await axios.get("http://localhost:3003/product_posts");
+      console.log(data.data);
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __updateproduct_posts = createAsyncThunk(
+  "Productposts/updateProductpost",
+  async (payload, thunkAPI) => {
+    try {
+      console.log(payload);
+      await axios.patch(
+        `http://localhost:3003/product_posts/${payload.id}`,
+        payload
+      );
+      const data = await axios.get("http://localhost:3003/product_posts");
+      console.log(data.data);
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const initialState = {
+  product_posts: [
+    {
+      id: 1,
+      title: "제목1",
+      content: "내용1",
+    },
+    {
+      id: 2,
+      title: "제목2",
+      content: "내용2",
+    },
+  ],
+  isLoading: false,
+  error: null,
+};
+
+const product_postsSlice = createSlice({
+  name: "product_posts",
+  initialState,
+  reducers: {
+    addpost: (state, action) => {
+      state.product_posts = [...state.product_posts, action.payload];
+    },
+
+    deletepost: (state, action) => {
+      state.product_posts = state.product_posts.filter(
+        (product_posts) => product_posts.id !== action.payload
+      );
+    },
+    togglepost: (state, action) => {
+      let postlist = state.product_posts.slice();
+      postlist.find((e) => e.id === action.payload).isDone = !postlist.find(
+        (e) => e.id === action.payload
+      ).isDone;
+      state.product_posts = postlist;
+    },
+    toggleDisplay: (state, action) => {
+      let postlist = state.product_posts.slice();
+      // console.log(postlist);
+      postlist.find((e) => e.id === action.payload).displaytoggle =
+        !postlist.find((e) => e.id === action.payload).displaytoggle;
+      state.product_posts = postlist;
+    },
+    updatepost: (state, action) => {
+      let postlist = state.product_posts.slice();
+      console.log(action.payload);
+      // postlist.find((e) => e.id === action.payload.id) =
+      //   action.payload;
+      // state.posts = postlist;
+    },
+  },
+  extraReducers: {
+    [__getProductpost.pending]: (state) => {
+      state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
+    },
+    [__getProductpost.fulfilled]: (state, action) => {
+      state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.product_posts = action.payload; // Store에 있는 posts에 서버에서 가져온 posts를 넣습니다.
+    },
+    [__getProductpost.rejected]: (state, action) => {
+      state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
+    },
+
+    [__addproduct_posts.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__addproduct_posts.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.product_posts = action.payload;
+    },
+    [__addproduct_posts.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+    [__deleteproduct_posts.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__deleteproduct_posts.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.product_posts = action.payload;
+    },
+    [__deleteproduct_posts.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+    [__updateproduct_posts.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__updateproduct_posts.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.product_posts = action.payload;
+    },
+    [__updateproduct_posts.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  addPost,
+  deleteproduct_posts,
+  toggleproduct_posts,
+  updateproduct_posts,
+  toggleDisplay,
+} = product_postsSlice.actions;
+// reducer 는 configStore에 등록하기 위해 export default 합니다.
+export default product_postsSlice.reducer;
