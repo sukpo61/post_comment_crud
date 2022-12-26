19bb8970b45b416bef1b80fe4af51b56d2a72d52
MarinaNam <namma123@naver.com>
2022-12-26 12:02:40 +0900

    merge용 MenuPage 2차

--- src/shared/router.js
+++ src/shared/router.js
@@ -2,27 +2,30 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PostsContainer from '../components/Pages/PostListpage/PostsContainer';
 import Header from '../components/Layout/Header';
-import Footer from '../components/Layout/Footer';
-import Mainpage from '../components/Pages/MainPage/Mainpage';
 import PostAddForm from '../components/Pages/PostAddpage/PostAddForm';
 import Postpage from '../components/Pages/Postpage/Postpage';
 import PostEditForm from '../components/Pages/PostEditpage/PostEditForm';
+import Footer from '../components/Layout/Footer';
+import ScrollToTop from '../components/Layout/ScrollToTop';
+import Mainpage from '../components/Pages/MainPage/Mainpage';
 import ProductPage from '../components/Pages/ProductPage/ProductPage';
 import MenuPage from '../components/Pages/MenuPage/MenuPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop></ScrollToTop>
       <Header></Header>
       <Routes>
         <Route path='/' element={<Mainpage />} />
-        <Route path='/productpage' element={<ProductPage />} />
+        <Route path='/reservation' element={<PostsContainer />} />
         <Route path='/addform' element={<PostAddForm />} />
         <Route path='/editform' element={<PostEditForm />} />
-        <Route path='/:id' element={<Postpage />} />
+        <Route path='/reservation/:id' element={<Postpage />} />
+        <Route path='/productpage/:id' element={<ProductPage />} />
         <Route path='/menu' element={<MenuPage />} />
       </Routes>
-      <Footer />
+      <Footer></Footer>
     </BrowserRouter>
   );
 };
