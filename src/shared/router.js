9bd7ba662c144756acb9f467b9329d28c18e6c42
prdg <prdg990515@gmail.com>
2022-12-26 12:02:46 +0900

    삭제할거 삭제후 router 수정

--- src/shared/router.js
+++ src/shared/router.js
@@ -5,19 +5,27 @@ import Header from "../components/Layout/Header";
 import PostAddForm from "../components/Pages/PostAddpage/PostAddForm";
 import Postpage from "../components/Pages/Postpage/Postpage";
 import PostEditForm from "../components/Pages/PostEditpage/PostEditForm";
+import Footer from "../components/Layout/Footer";
+import ScrollToTop from "../components/Layout/ScrollToTop";
+import Mainpage from "../components/Pages/MainPage/Mainpage";
 import ProductPage from "../components/Pages/ProductPage/ProductPage";
+import MenuPage from "../components/Pages/MenuPage/MenuPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop></ScrollToTop>
       <Header></Header>
       <Routes>
-        <Route exact path="/" element={<PostsContainer />} />
-        <Route path="/productpage/:id" element={<ProductPage />} />
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/reservation" element={<PostsContainer />} />
         <Route path="/addform" element={<PostAddForm />} />
         <Route path="/editform" element={<PostEditForm />} />
-        <Route exact path="/:id" element={<Postpage />} />
+        <Route path="/reservation/:id" element={<Postpage />} />
+        <Route path="/productpage/:id" element={<ProductPage />} />
+        <Route path="/menu" element={<MenuPage />} />
       </Routes>
+      <Footer></Footer>
     </BrowserRouter>
   );
 };
