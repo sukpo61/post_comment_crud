e1573bb5230b8ba95961dbd66118f031aeab9c8e
MarinaNam <namma123@naver.com>
2022-12-27 15:47:42 +0900

    constant 배포 주소 설정

--- src/redux/modules/constant.js
+++ src/redux/modules/constant.js
@@ -1,2 +1,3 @@
 // export const AXIOS_ADDRESS = 'http://localhost:3003';
-export const AXIOS_ADDRESS = 'null-bread-x89h-7h435zzka-nyangmal123.vercel.app';
+export const AXIOS_ADDRESS =
+  'nullbread-server-fdlzg7ivx-nyangmal123.vercel.app';
